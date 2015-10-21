f5c028e0da15eddb73c5d5238fed62a76e267d0c
valentinconan <valentin.conan@me.com>
2015-10-21 19:48:41 +0200

    ajout des XMLelement pour la gestion des nom dans le JSON retourne par le service

--- src/main/java/InvoiceRestService.java
+++ src/main/java/InvoiceRestService.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -31,10 +34,8 @@ public class InvoiceRestService {
 	@Path("/post")
 	@Consumes("application/json")
 	public Response createInvoiceJSON(Invoice invoice) {
-
 		String result = "Product created : " + invoice;
 		return Response.status(201).entity(result).build();
-		
 	}
 	
 	/**
@@ -45,12 +46,25 @@ public class InvoiceRestService {
 	@Path("/get")
 	@Produces("application/json")
 	public Invoice getInvoiceJSON() {
-
 		Invoice lInvoice = new Invoice();
 		lInvoice.setName("iPad 3");
 		lInvoice.setPrice(999);
-		
 		return lInvoice; 
-
+	}
+	
+	/**
+	 * Returns an Invoice
+	 * @return the invoice
+	 */
+	@GET
+	@Path("/getMulti")
+	@Produces("application/json")
+	public List<Invoice> getInvoicesJSON() {
+		List<Invoice> lInvoices = new ArrayList<Invoice>();
+		lInvoices.add(new Invoice("Ipad 1",600));
+		lInvoices.add(new Invoice("Ipad 2",700));
+		lInvoices.add(new Invoice("Ipad 3",800));
+		lInvoices.add(new Invoice("Ipad Air",1000));
+		return lInvoices; 
 	}
 }
--- src/main/java/com/rest/Invoice.java
+++ src/main/java/com/rest/Invoice.java
@@ -1,10 +1,22 @@
 package com.rest;
 
+import javax.xml.bind.annotation.XmlElement;
+
+@SuppressWarnings("restriction")
 public class Invoice {
 
+	@XmlElement(name="name")
 	String name;
+	@XmlElement(name="price")
 	int price;
 	
+	public Invoice(){
+		
+	}
+	public Invoice(String aName, int aPrice){
+		this.name=aName;
+		this.price=aPrice;
+	}
 	public String getName() {
 		return name;
 	}
